baf90af61e2394ebe16f6dc03e98371f97602b41
xamthor <miguel@mhernandez.me>
2021-11-21 04:32:23 -0600

    CORS

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-  
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+	const configService = app.get(ConfigService);
+	app.enableCors({ origin: configService.get('ORIGIN')});
+	await app.listen(3000);
 }
 bootstrap();
